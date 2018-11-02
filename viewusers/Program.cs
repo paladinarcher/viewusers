059b9a94cf3bf4c6632256001b118ed4ed5f5996
tina <tmorozco1990@gmail.com>
2018-11-02 16:51:48 -0600

    add readme file and fix assemblyname information

--- viewusers/Program.cs
+++ viewusers/Program.cs
@@ -19,13 +19,6 @@ namespace ViewUsers
          }
        );
 
-      // connect to db 
-      // var client = new MongoClient("mongodb://localhost:3001/");
-      // var db = client.GetDatabase("meteor");
-      // var collection = db.GetCollection<Users>("users");
-      // var users = collection.Find(_ => true).ToList().ToJson();
-      // Console.WriteLine(collection.Find(_ => true).ToList().ToJson());
-
       // serve w/ nancy
       var uri = "http://localhost:3002";
       Console.WriteLine("Starting Nancy on " + uri);
